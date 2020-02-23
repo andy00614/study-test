979f595c5f1235d6b6f4305ff92926b39511b486
andy00614 <33732843@qq.com>
2020-02-23 12:59:56 +0800

    test async correct

--- fetchData.test.js
+++ fetchData.test.js
@@ -1,7 +1,12 @@
 import { requestMock } from './fetchData'
 
-test('test async function',() => {
+test('test async function',(done) => {
+
   requestMock().then(res => {
-    expect(res).toBe(10)
+    // 这里如果是promise的话加return,否则的话返回不出去，按没有结果来处理
+    // 没有结果的话那肯定就是默认通过了
+    // 或者用done
+    return expect(res).toBe('true2222')
+    // done()
   })
 })
