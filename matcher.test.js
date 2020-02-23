bcb825748f5c8813f4a3cd4ca457905bad56eaef
andy00614 <33732843@qq.com>
2020-02-23 12:17:31 +0800

    test bash

---
+++ matcher.test.js
@@ -0,0 +1,25 @@
+test("matcher", () => {
+  const a = {
+    one: {
+      b: 1
+    }
+  };
+  expect(a).toEqual({
+    one: {
+      b: 1
+    }
+  });
+});
+ 
+// String
+test("toMatch", () => {
+  const  str = "abcd";
+  // 或者正则表达式 
+  expect(str).toMatch("abc");
+});
+
+// Array,Set
+test('toContainer',() => {
+  const arr = ['andy',1,23,3]
+  expect(arr).toContain(3)
+})
--- math.test.js
+++ math.test.js
@@ -4,10 +4,10 @@ test('测试加法 3 + 7',() => {
   expect(add(3,7)).toBe(10)
 })
 
-test('测试减法 3 - 7',() => {
-  expect(minus(3,7)).toBe(-4)
-})
+// test('测试减法 3 - 7',() => {
+//   expect(minus(3,6)).toBe(-3)
+// })
 
-test('测试乘法 3 * 3',() => {
-  expect(multi(3,3)).toBe(9)
-})
+// test('测试乘法 3 * 3',() => {
+//   expect(multi(3,3)).toBe(9)
+// })
